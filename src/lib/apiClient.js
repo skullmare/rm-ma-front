83dc442c9c3e7b261939a5bc8b00196c393bb756
Ivan Ostapchuk <n8n.shkuratetskiyav@gmail.com>
2026-01-20 18:31:23 +0300

    commit 20/01/26 tasks

--- src/lib/apiClient.js
+++ src/lib/apiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Для Vite используем import.meta.env
-const baseURL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:4000';
+const baseURL = "https://rm-ma-back-rocketmind.amvera.io";
 
 const apiClient = axios.create({
   baseURL: baseURL,
